356ebe0bff20abb5c7fb976e7b02caac8e17c87c
Jess Sullivan <jess@sulliwood.org>
2026-05-02 22:51:56 -0400

    Isolate FileProvider lab checkout git config

--- scripts/test-release-workflow-fileprovider.sh
+++ scripts/test-release-workflow-fileprovider.sh
@@ -189,6 +189,8 @@ check_testing_mode_package_workflow() {
   assert_contains "$TESTING_MODE_PKG_WORKFLOW" "runner_label:"
   assert_contains "$TESTING_MODE_PKG_WORKFLOW" 'default: "petting-zoo-mini"'
   assert_contains "$TESTING_MODE_PKG_WORKFLOW" "auto-development"
+  assert_contains "$TESTING_MODE_PKG_WORKFLOW" "GIT_CONFIG_GLOBAL: /dev/null"
+  assert_contains "$TESTING_MODE_PKG_WORKFLOW" "GIT_CONFIG_NOSYSTEM"
   assert_contains "$TESTING_MODE_PKG_WORKFLOW" "signing_keychain:"
   assert_contains "$TESTING_MODE_PKG_WORKFLOW" "TCFS_CODESIGN_KEYCHAIN"
   assert_contains "$TESTING_MODE_PKG_WORKFLOW" "Apple Development"
